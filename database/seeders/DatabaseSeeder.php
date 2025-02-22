3d260e839efeb446519bbe8a5d178c630f1aa876
kimnopal <naufalhakim366@gmail.com>
2025-02-23 00:50:10 +0700

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -183,23 +183,23 @@ class DatabaseSeeder extends Seeder
         ]);
 
         // Laporan
-        Report::create([
-            'user_id' => 1,
-            'service_id' => 1,
-            'name' => 'Naufal',
-            'email' => 'naufal@gmail.com',
-            'no_handphone' => '081234567890',
-            'description' => 'lorem ipsum dolor sit amet',
-        ]);
+        // Report::create([
+        //     'user_id' => 1,
+        //     'service_id' => 1,
+        //     'name' => 'Naufal',
+        //     'email' => 'naufal@gmail.com',
+        //     'no_handphone' => '081234567890',
+        //     'description' => 'lorem ipsum dolor sit amet',
+        // ]);
 
-        Report::create([
-            'user_id' => 5,
-            'service_id' => 1,
-            'name' => 'Naufal',
-            'email' => 'naufal@gmail.com',
-            'no_handphone' => '081234567890',
-            'description' => 'Test deskripsi laporan',
-        ]);
+        // Report::create([
+        //     'user_id' => 5,
+        //     'service_id' => 1,
+        //     'name' => 'Naufal',
+        //     'email' => 'naufal@gmail.com',
+        //     'no_handphone' => '081234567890',
+        //     'description' => 'Test deskripsi laporan',
+        // ]);
 
         Report::factory(1000)->create();
     }
